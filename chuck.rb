911e5b5cbae25b0befa27c0e0ca803dddb38aa6d
Ajaeb Ismael <ajaebabdurahman@outlook.com>
2019-06-07 16:44:36 -0400

    colorized it

--- chuck.rb
+++ chuck.rb
@@ -1,14 +1,14 @@
 require 'chuck_norris'
+require 'colorized_string'
 # require './app'
 class ChuckJokes
   def initialize()
   end
 
   def help
-    puts "Run [ruby app.rb random] to give you a random joke."
-    puts "Run [ruby app.rb {IdNumber} ] to give you a joke with the id you input."
-    puts "Run [ruby app.rb {Name}] to give you a joke with changing the name."
-
+    puts ColorizedString["Run [ruby app.rb random] to give you a random joke."].colorize(:red)
+    puts ColorizedString["Run [ruby app.rb {IdNumber} ] to give you a joke with the id you input."].colorize(:red)
+    puts ColorizedString["Run [ruby app.rb {Name}] to give you a joke with changing the name."].colorize(:red)
   end
 
   def get_joke_by_id(id)
@@ -16,8 +16,8 @@ class ChuckJokes
     if joking.id == 'id'
       puts "A joke with that id doesnt exist."
     else
-      puts joking.joke
-      puts joking.id
+      puts ColorizedString[joking.joke].colorize(:green)
+      puts ColorizedString[joking.id.to_s].colorize(:green)
     end
   end
 
@@ -25,8 +25,8 @@ class ChuckJokes
     nam = name.split()
     if name.split().length == 2
       joking = ChuckNorris::JokeFinder.get_joke(first_name: "#{nam[0]}", last_name: "#{nam[1]}" )
-      puts joking.joke
-      puts joking.id
+      puts ColorizedString[joking.joke].colorize(:green)
+      puts ColorizedString[joking.id.to_s].colorize(:green)
     else
       puts help
     end
@@ -34,7 +34,7 @@ class ChuckJokes
 
   def random
     joking = ChuckNorris::JokeFinder.get_joke
-    puts joking.joke
-    puts joking.id
+    puts ColorizedString[joking.joke].colorize(:green)
+    puts ColorizedString[joking.id.to_s].colorize(:green)
   end
 end
